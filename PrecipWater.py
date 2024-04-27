acec68bd10db3624c331c199e1d8d49b7222edf5
skg226 <167796513+skg226@users.noreply.github.com>
2024-04-27 16:13:10 -0500

    Add files via upload

--- PrecipWater.py
+++ PrecipWater.py
@@ -43,12 +43,59 @@ cbar = plt.colorbar(contour, ax=ax, orientation='horizontal', pad=0.05)
 cbar.set_label('Precipitable Water (inches)')
 
 # Add title
-plt.title('GFS Forecast Precipitable Water 08-11-2016')
+plt.title('GFS 00Z Precipitable Water 08-11-2016')
 
 # Show the plot
 plt.show()
 
 # Close the GRIB file
 grbGFS.close()
+
+#Precip Water Zoomed in
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.feature as cf
+import cartopy.crs as ccrs
+import pygrib
+
+# Open the GRIB file
+grbGFS = pygrib.open('/Users/sydneygoldberg/Desktop/CM_Project/gfs.0p25.2016081100.f000.grib2')
+
+# Select the variable for Precipitable Water
+grbGFS.select(name='Precipitable water')
+precipwater = grbGFS[274]
+pw = precipwater['values'] * 0.0393701  # Convert kg/m² to inches
+
+lats, lons = precipwater.latlons()
+
+# Set up the plot
+fig = plt.figure(figsize=(8, 8))
+proj = ccrs.LambertConformal(central_longitude=-96., central_latitude=40., standard_parallels=(40., 40.))
+ax = plt.axes(projection=proj)
+ax.set_extent([-95.75, -84.75, 27.75, 34.75])
+
+# Add map features
+ax.add_feature(cf.LAND, color='white')
+ax.add_feature(cf.OCEAN, color='white')
+ax.add_feature(cf.COASTLINE, edgecolor='black')
+ax.add_feature(cf.BORDERS, edgecolor='black')
+ax.add_feature(cf.STATES, edgecolor='black')
+ax.add_feature(cf.LAKES, alpha=0.5, color='white')
+
+# Plot precipitable water
+levels = np.arange(0, 3.3, 0.1)  
+contour = ax.contourf(lons, lats, pw, levels, cmap=plt.cm.gist_earth_r, transform=ccrs.PlateCarree())
+cbar = plt.colorbar(contour, ax=ax, orientation='horizontal', pad=0.05)
+cbar.set_label('Precipitable Water (inches)')
+
+# Add title
+plt.title('GFS 00Z Precipitable Water 08-11-2016')
+
+# Show the plot
+plt.show()
+
+# Close the GRIB file
+grbGFS.close()
+
 
 
